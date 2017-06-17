0ef7ed4aebb4f51afa0a42f165fcdd223700700f
aceralon <aceralon@outlook.com>
2017-06-17 20:57:36 +0800

    Seems finished

--- classify.m
+++ classify.m
@@ -1,5 +1,18 @@
-function [fs1] = classify(x)
-%     fs1 = abs(MMk(x, 8, 0))/MMk(x, 2,1).^4;
+function [type] = classify(x)
+type = -1;
+% 1. BPSK; 2. QPSK; 3.8QAM; 4. 16QAM; 5. 32QAM; 6.64QAM; 7.2FSK; 
+%8.4FSK; 9.8FSK;  10.4ASK; 11.8ASK
+%type1th
+%272.0000   33.9599   43.5059   13.9654    1.9869   11.5302    0.5976
+%0.0469    0.0106  48.2447   66.1035
+
+th1 = [272.0000   33.9622   43.5034   13.9617 11.5009  48.2853   66.0788 0];
+%1. BPSK; 2. QPSK; 3.8QAM; 4. 16QAM; 5.64QAM 6.4ASK; 7.8ASK 8.MFSK&32QAM
+
+th2 = [15.3843    0.5929    5.6314]; %1.2FSK 2.4FSK 3.8FSK&32QAM
+
+th3 = [3.2305 0];%1.32QAM 2.8FSK
+
     m80 = MMk(x, 8, 0);
     m60 = MMk(x, 6, 0);
     m40 = MMk(x, 4, 0);
@@ -11,17 +24,57 @@ function [fs1] = classify(x)
     
     fs1 = abs(c80)/(abs(c21)^4);
     
-    xdm = medfilt1(abs(diff(x)));
+    diff1 = abs(th1-fs1);
+    fil1 = find(diff1 == min(diff1));
     
-    md42 = MMk(xdm, 4, 2);
-    md20 = MMk(xdm ,2, 0);
-    md21 = MMk(xdm, 2, 1);
-    md63 = MMk(xdm, 6, 3);
-    md41 = MMk(xdm, 4, 1);
+    if fil1 ~= 8
+        switch fil1
+            case 1
+                type = 1;
+            case 2
+                type = 2;
+            case 3
+                type = 3;
+            case 4
+                type = 4;
+            case 5
+                type = 6;
+            case 6
+                type = 10;
+            case 7
+                type = 11;
+        end
+    else
+        xdm = diff(x);
     
-    cd42 = md42 - abs(md20)^2 - 2*md21^2;
-    cd63 = md63 - 6*md41*md20 - 9*md42*md21 + 18*md20^2*md21 + 12*md21^3;
-    
-    fs2 = (abs(cd63)^2)/(abs(cd42)^3);
-    
+        md42 = MMk(xdm, 4, 2);
+        md20 = MMk(xdm ,2, 0);
+        md21 = MMk(xdm, 2, 1);
+        md63 = MMk(xdm, 6, 3);
+        md41 = MMk(xdm, 4, 1);
+
+        cd42 = md42 - abs(md20)^2 - 2*md21^2;
+        cd63 = md63 - 6*md41*md20 - 9*md42*md21 + 18*md20^2*md21 + 12*md21^3;
+
+        fs2 = (abs(cd63)^2)/(abs(cd42)^3);
+        
+        diff2 = abs(th2-fs2);
+        fil2 = find(diff2 == min(diff2));
+        
+        if fil2 ~=3
+            type = fil2 + 6;
+        else
+            xt = real(x);
+            yt = imag(x);
+            instantAmplitude = sqrt(xt.^2 + yt.^2);
+            AmplitudeMean = mean( ( instantAmplitude - 1 ) );  
+            diff3 = abs(th3 - AmplitudeMean);
+            fil1 = find(diff3 == min(diff3));
+            if fil1 == 1
+                type = 5;
+            else
+                type = 9;
+            end
+        end        
+    end
 end
--- sg.m
+++ sg.m
@@ -3,115 +3,125 @@
 figure;
 
 
-lineStyle = ['o-';  'h-';  '*-';  '.-';  'x-';  's-';  'd-';  '^-';  'p-'; '*-'; '*-'];
+lineStyle = ['o-';  'h-';  '*-';  '.-';  'x-';  's-';  'd-';  '^-';  'p-'; '+-'; '<-'];
 
-% signal generation;Â¶ÇÊûúÊÉ≥Ë¶ÅËøõË°å100ÁªÑÁã¨Á´ãÁöÑÊµãËØïÔºåÂèØ‰ª•Âª∫Á´ã100Ê¨°Âæ™ÁéØÔºå‰∫ßÁîü100ÁªÑÁã¨Á´ãÁöÑÊï∞ÊçÆ
+% signal generation;»Áπ˚œÎ“™Ω¯––100◊È∂¿¡¢µƒ≤‚ ‘£¨ø…“‘Ω®¡¢100¥Œ—≠ª∑£¨≤˙…˙100◊È∂¿¡¢µƒ ˝æ›
 the = zeros(1,11);
 for j = 1:11  % bit per symbol: 1. BPSK; 2. QPSK; 3.8QAM; 4. 16QAM; 5. 32QAM; 6.64QAM; 7.2FSK; 8.4FSK; 9.8FSK; 10.4ASK; 11.8ASK
 
-    System.BitPerSymbol = j;
-    snr = 0:20;  %SNR‰ø°Âô™ÊØîÁöÑËÆæÁΩÆÔºåÂçï‰ΩçdB
-%     figure;
+    if j < 10
+        System.BitPerSymbol = j;
+    else
+        System.BitPerSymbol = j - 8;
+    end
+    snr = -5:50;  %SNR–≈‘Î±»µƒ…Ë÷√£¨µ•ŒªdB
     fs1 = zeros(1,16);
+
+    acc = zeros(1, length(snr));
     for snrIndex= 1:length(snr)
+        cnt = 0;
+        for repeat = 1:100
+            Tx.SampleRate = 32e9; %symbol Rate£¨–≈∫≈µƒ¬Î‘™ÀŸ¬ £¨ø…“‘◊‘––∂®“Â
+            Tx.Linewidth = 0;%∑¢…‰–≈∫≈µƒ‘ÿ≤®µƒœﬂøÌ£¨“ª∞„”Î–≈∫≈µƒœ‡Œª‘Î…˘”–πÿ£¨¥Û–°ø…◊‘––…Ë÷√£¨’‚¿Ô‘› ±…Ë÷√Œ™0
+            Tx.Carrier = 0;%∑¢…‰–≈∫≈µƒ‘ÿ≤®∆µ¬ £¨ø…◊‘––…Ë÷√£¨’‚¿Ô‘›…ËŒ™0
+            M = 2^System.BitPerSymbol;
 
-        Tx.SampleRate = 32e9; %symbol RateÔºå‰ø°Âè∑ÁöÑÁ†ÅÂÖÉÈÄüÁéáÔºåÂèØ‰ª•Ëá™Ë°åÂÆö‰πâ
-        Tx.Linewidth = 0;%ÂèëÂ∞Ñ‰ø°Âè∑ÁöÑËΩΩÊ≥¢ÁöÑÁ∫øÂÆΩÔºå‰∏ÄËà¨‰∏é‰ø°Âè∑ÁöÑÁõ∏‰ΩçÂô™Â£∞ÊúâÂÖ≥ÔºåÂ§ßÂ∞èÂèØËá™Ë°åËÆæÁΩÆÔºåËøôÈáåÊöÇÊó∂ËÆæÁΩÆ‰∏∫0
-        Tx.Carrier = 0;%ÂèëÂ∞Ñ‰ø°Âè∑ÁöÑËΩΩÊ≥¢È¢ëÁéáÔºåÂèØËá™Ë°åËÆæÁΩÆÔºåËøôÈáåÊöÇËÆæ‰∏∫0
-        M = 2^System.BitPerSymbol;
+            if j == 7 || j == 8 || j == 9 %fsk
+                Tx.DataSymbol = randi([0 M/64-1],1,10000);
+            else
+                Tx.DataSymbol = randi([0 M-1],1,10000);%√ø“ª¥ŒÀÊª˙≤˙…˙µƒ ˝æ›¡ø£¨’‚¿Ô‘› ±…ËŒ™ ˝æ›µ„∏ˆ ˝Œ™10000∏ˆ
+            end
 
-        if j == 7 || j == 8 || j == 9 %fsk
-            Tx.DataSymbol = randi([0 M/64-1],1,10000);
-        else
-            Tx.DataSymbol = randi([0 M-1],1,10000);%ÊØè‰∏ÄÊ¨°ÈöèÊú∫‰∫ßÁîüÁöÑÊï∞ÊçÆÈáèÔºåËøôÈáåÊöÇÊó∂ËÆæ‰∏∫Êï∞ÊçÆÁÇπ‰∏™Êï∞‰∏∫10000‰∏™
-        end
-        %Êï∞ÊçÆÁöÑ‰∏çÂêåË∞ÉÂà∂ÊñπÂºè‰∫ßÁîüÔºöËøôÈáåÊää2^3Ôºà8QAMÔºâÁöÑÂΩ¢ÂºèÂçïÁã¨ÊãøÂá∫Êù•ËÆæÁΩÆÔºåÊòØ‰∏∫‰∫ÜÂÆûÁé∞ÊúÄ‰ºòÁöÑÊòüÂûã8QAMÊòüÂ∫ßÂõæ
-        if j == 7 || j == 8 || j == 9 %fsk            
-            Tx.DataConstel = fskmod(Tx.DataSymbol,M/64,50,j,15000);
-        elseif j > 9
-            Tx.DataConstel = Tx.DataSymbol;
-%             for kk = 1:length(Tx.DataSymbol)
-%                 Tx.DataConstel(kk) = Tx.DataSymbol(kk) - M/2;
-%                 if(Tx.DataSymbol(kk) >= M/2)
-%                     Tx.DataConstel(kk) = Tx.DataConstel(kk) + 1;
-%                 end
-%             end
-        elseif M ~= 8
-
-%             h = modem.qammod('M', M, 'SymbolOrder', 'Gray');
-%             Tx.DataConstel = modulate(h,Tx.DataSymbol);
-                Tx.DataConstel = qammod(Tx.DataSymbol, M, 'gray');
-        else
-                tmp = Tx.DataSymbol;
-                tmp2  = zeros(1,length(Tx.DataSymbol));
-                for kk = 1:length(Tx.DataSymbol)
-                    switch tmp(kk)
-                        case 0
-                            tmp2(kk) = 1 + 1i;
-                        case 1
-                            tmp2(kk) = -1 + 1i;
-                        case 2
-                            tmp2(kk) = -1 - 1i;
-                        case 3
-                            tmp2(kk) = 1 - 1i;
-                        case 4
-                            tmp2(kk) = 1+sqrt(3);
-                        case 5
-                            tmp2(kk) = 0 + 1i .* (1+sqrt(3));
-                        case 6
-                            tmp2(kk) = 0 - 1i .* (1+sqrt(3));
-                        case 7
-                            tmp2(kk) = -1-sqrt(3);
+            % ˝æ›µƒ≤ªÕ¨µ˜÷∆∑Ω Ω≤˙…˙£∫’‚¿Ô∞—2^3£®8QAM£©µƒ–Œ Ωµ•∂¿ƒ√≥ˆ¿¥…Ë÷√£¨ «Œ™¡À µœ÷◊Ó”≈µƒ–«–Õ8QAM–«◊˘Õº
+            if j == 7 || j == 8 || j == 9 %fsk            
+                Tx.DataConstel = fskmod(Tx.DataSymbol,M/64,50,j,15000);
+            elseif j > 9
+                Tx.DataConstel = Tx.DataSymbol;
+            elseif M ~= 8
+    %             h = modem.qammod('M', M, 'SymbolOrder', 'Gray');
+    %             Tx.DataConstel = modulate(h,Tx.DataSymbol);
+                    Tx.DataConstel = qammod(Tx.DataSymbol, M, 'gray');
+            else
+                    tmp = Tx.DataSymbol;
+                    tmp2  = zeros(1,length(Tx.DataSymbol));
+                    for kk = 1:length(Tx.DataSymbol)
+                        switch tmp(kk)
+                            case 0
+                                tmp2(kk) = 1 + 1i;
+                            case 1
+                                tmp2(kk) = -1 + 1i;
+                            case 2
+                                tmp2(kk) = -1 - 1i;
+                            case 3
+                                tmp2(kk) = 1 - 1i;
+                            case 4
+                                tmp2(kk) = 1+sqrt(3);
+                            case 5
+                                tmp2(kk) = 0 + 1i .* (1+sqrt(3));
+                            case 6
+                                tmp2(kk) = 0 - 1i .* (1+sqrt(3));
+                            case 7
+                                tmp2(kk) = -1-sqrt(3);
+                        end
                     end
-                end
-                Tx.DataConstel = tmp2;
-                clear tmp tmp2;
+                    Tx.DataConstel = tmp2;
+                    clear tmp tmp2;
+            end
+
+            Tx.Signal = Tx.DataConstel;
+
+            % ˝æ›µƒ‘ÿ≤®º”‘ÿ£¨øº¬«µΩœ‡Œª‘Î…˘µ»
+            N = length(Tx.Signal);
+            dt = 1/Tx.SampleRate;
+            t = dt*(0:N-1);
+            Phase1 = [0, cumsum(normrnd(0,sqrt(2*pi*Tx.Linewidth/(Tx.SampleRate)), 1, N-1))];
+            carrier1 = exp(1i*(2*pi*t*Tx.Carrier + Phase1));
+            Tx.Signal = Tx.Signal.*carrier1;
+
+            if(j == 1 || j >=10 )
+                Tx.Signal = complex(Tx.Signal);
+            end
+
+            Rx.Signal = awgn(Tx.Signal,snr(snrIndex),'measured');% ˝æ›‘⁄AWGN–≈µ¿œ¬µƒΩ” ’
+%             Rx.Signal = Tx.Signal;
+
+            CMAOUT = Rx.Signal;
+
+            %normalizationΩ” ’–≈∫≈π¶¬ πÈ“ªªØ
+    %         CMAOUT=CMAOUT/sqrt(mean(abs(CMAOUT).^2));
+
+            type = classify(Rx.Signal);
+            
+            if type == j
+                cnt = cnt +1;
+            end
+             
+%             subplot(1,4,snrIndex); 
+%             plot(Rx.Signal,'.');
+
+%             fs1(snrIndex) = classify(Rx.Signal);
+
+    %         si = [real(Rx.Signal)' imag(Rx.Signal)'];
+    %         center = subclust(si, [0.1 0.1]);
+    %         hold on;
+    %         center = complex(center( : , 1)', center(:,2)');
+    %         plot(center,'*');
+    %         options = statset('MaxIter',1000);
+    %         gmfit = fitgmdist(si,3,'CovarianceType','diagonal',...
+    %             'SharedCovariance','true','Options',options);
+    %         center = cluster(gmfit, si);
+    %         hold on;
+    %         plot(center, '.');
+        
         end
-        
-        Tx.Signal = Tx.DataConstel;
-
-        %Êï∞ÊçÆÁöÑËΩΩÊ≥¢Âä†ËΩΩÔºåËÄÉËôëÂà∞Áõ∏‰ΩçÂô™Â£∞Á≠â
-        N = length(Tx.Signal);
-        dt = 1/Tx.SampleRate;
-        t = dt*(0:N-1);
-        Phase1 = [0, cumsum(normrnd(0,sqrt(2*pi*Tx.Linewidth/(Tx.SampleRate)), 1, N-1))];
-        carrier1 = exp(1i*(2*pi*t*Tx.Carrier + Phase1));
-        Tx.Signal = Tx.Signal.*carrier1;
-
-        if(j == 1 || j >=8 )
-            Tx.Signal = complex(Tx.Signal);
-        end
-        
-        Rx.Signal = awgn(Tx.Signal,snr(snrIndex),'measured');%Êï∞ÊçÆÂú®AWGN‰ø°ÈÅì‰∏ãÁöÑÊé•Êî∂
-%         Rx.Signal = Tx.Signal;
-
-        CMAOUT = Rx.Signal;
-
-        %normalizationÊé•Êî∂‰ø°Âè∑ÂäüÁéáÂΩí‰∏ÄÂåñ
-        CMAOUT=CMAOUT/sqrt(mean(abs(CMAOUT).^2));
-
-%         subplot(1,4,snrIndex); 
-%         plot(Rx.Signal,'.');
-
-        fs1(snrIndex) = classify(Rx.Signal);
-                
-%         si = [real(Rx.Signal)' imag(Rx.Signal)'];
-%         center = subclust(si, [0.1 0.1]);
-%         hold on;
-%         center = complex(center( : , 1)', center(:,2)');
-%         plot(center,'*');
-%         options = statset('MaxIter',1000);
-%         gmfit = fitgmdist(si,3,'CovarianceType','diagonal',...
-%             'SharedCovariance','true','Options',options);
-%         center = cluster(gmfit, si);
-%         hold on;
-%         plot(center, '.');
-        
-    end    
-    plot(fs1,lineStyle(j,:));
-    grid on
+        acc(snrIndex) = cnt/100;
+%     plot(fs1,lineStyle(j,:));
+%     the(j) = mean(fs1);
+    end
     hold on
-    the(j) = mean(fs1);
+    plot(snr, acc,lineStyle(j,:));
+    axis([0 32 0 1]);
 end
+grid on
 legend('BPSK', 'QPSK', '8QAM', '16QAM', '32QAM', '64QAM', '2FSK', '4FSK', '8FSK', '4ASK', '8ASK');
-
+% legend( 'QPSK', '8QAM', '16QAM', '32QAM', '64QAM', '2FSK', '4FSK', '8FSK', '4ASK', '8ASK');
